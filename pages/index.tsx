563cc89a36fed5a348ba4e17e9a0ee37032b9995
RedRe4per <s3582474@gmail.com>
2023-02-20 18:52:18 +1100

    Hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import Header from '@/layouts/Header'
 import Footer from '@/layouts/Footer'
+import Hero from '@/components/Hero'
 
 
 export default function Home() {
   return (
     <main className="wrapper">
       <Header/>
-      <div>uat test</div>
+      <Hero/>
       <Footer/>
     </main>
   )
